2716b3cdbfc64ffe6f3c1247bb684d305a907485
Toshiro58 <dorog2283222@gmail.com>
2024-09-29 20:48:07 +0300

    Update modAlphaCipher.h

--- modAlphaCipher.h
+++ modAlphaCipher.h
@@ -1,40 +1,22 @@
-#include "modAlphaCipher.h"
+#pragma once
 
-modAlphaCipher::modAlphaCipher(const std::wstring& skey) {
-    for (unsigned i = 0; i < numAlpha.size(); i++) {
-        alphaNum[numAlpha[i]] = i;
-    }
-    key = convert(skey);
-}
+#include <map>
+#include <string>
+#include <vector>
 
-std::wstring modAlphaCipher::encrypt(const std::wstring& open_text) {
-    std::vector<int> work = convert(open_text);
-    for (unsigned i = 0; i < work.size(); i++) {
-        work[i] = (work[i] + key[i % key.size()]) % alphaNum.size();
-    }
-    return convert(work);
-}
+class modAlphaCipher {
+private:
+    std::wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    std::map<char, int> alphaNum;
+    std::vector<int> key;
 
-std::wstring modAlphaCipher::decrypt(const std::wstring& cipher_text) {
-    std::vector<int> work = convert(cipher_text);
-    for (unsigned i = 0; i < work.size(); i++) {
-        work[i] = (work[i] + alphaNum.size() - key[i % key.size()]) % alphaNum.size();
-    }
-    return convert(work);
-}
+    std::vector<int> convert(const std::wstring& s);
+    std::wstring convert(const std::vector<int>& v);
 
-inline std::vector<int> modAlphaCipher::convert(const std::wstring& s) {
-    std::vector<int> result;
-    for (auto c : s) {
-        result.push_back(alphaNum[c]);
-    }
-    return result;
-}
-
-inline std::wstring modAlphaCipher::convert(const std::vector<int>& v) {
-    std::wstring result;
-    for (auto i : v) {
-        result.push_back(numAlpha[i]);
-    }
-    return result;
-}
+public:
+    modAlphaCipher() = delete;
+    modAlphaCipher(const std::wstring& skey);
+    
+    std::wstring encrypt(const std::wstring& open_text);
+    std::wstring decrypt(const std::wstring& cipher_text);
+};
